1523a75891982047a0083fc63e389e9e3998c1d7
Your Name <you@example.com>
2023-11-17 14:19:02 -0300

    refactor: Formatação total

--- index.js
+++ index.js
@@ -1,85 +1,59 @@
+const btnON = document.getElementById("btn-ON"),
+  btnOFF = document.getElementById("btn-OFF"),
+  btnVERIFY = document.getElementById("btn-VERIFY");
+
+const urlServer = "ws://192.168.43.100:81";
+const browserSupportsWebsocket = "WebSocket" in window;
+
 let controller = {
   data: null,
   get message() {
     if (!this.data) {
       return `não tem dados`;
     }
+
     let last = this.data[this.data.length - 1];
-    console.log(last);
-    return `corrente: ${last.corrente}\n potencia: ${last.potencia}`;
+
+    return `corrente: ${last.corrente} A \n potencia: ${last.potencia} W`;
   },
 };
 
-document.addEventListener("DOMContentLoaded", function () {
-  if ("WebSocket" in window) {
-    // O navegador suporta WebSocket
-    let webSocket = new WebSocket("ws://192.168.43.100:81/");
+if (browserSupportsWebsocket) {
+  let connection = new WebSocket(urlServer);
 
-    webSocket.onmessage = (event) => {
-      let data = JSON.parse(event.data);
-      if (!controller.data) {
-        controller.data = [];
-      }
-      controller.data.push(data);
-
-      console.table(controller.data, ["corrente", "potencia"]);
-    };
-
-    webSocket.addEventListener("open", function (event) {
-      console.log("Conexão WebSocket aberta");
-
-      // Adicione a lógica de enviar comandos quando os botões são clicados
-      document
-        .getElementById("btn-ligar")
-        .addEventListener("click", function () {
-          enviarComando("L");
-        });
-
-      document
-        .getElementById("btn-desligar")
-        .addEventListener("click", function () {
-          enviarComando("D");
-        });
-
-      document
-        .getElementById("btn-verificarconsumo")
-        .addEventListener("click", function () {
-          alert(controller.message);
-        });
-
-      function enviarComando(comando) {
-        webSocket.send(comando);
-      }
-    });
-
-    webSocket.addEventListener("close", function (event) {
-      console.log("Conexão WebSocket fechada");
-    });
-
-    webSocket.addEventListener("error", function (event) {
-      console.error("Erro WebSocket:", event);
-    });
-  } else {
-    // O navegador não suporta WebSocket
-    console.error("Seu navegador não suporta WebSocket.");
-  }
-});
-
-// Função para processar as mensagens recebidas do Arduino
-function processarMensagem(mensagem) {
-  // Converta a mensagem para um objeto JSON
-  var dados = JSON.parse(mensagem);
-
-  // Atualize o estado do relé
-  if (dados.ligado) {
-    document.getElementById("status-rele").textContent = "Ligado";
-  } else {
-    document.getElementById("status-rele").textContent = "Desligado";
+  function sendRequest(comando) {
+    connection.send(comando);
   }
 
-  // Atualize o consumo de energia
-  document.getElementById("consumo-energia").textContent = dados.consumo;
+  connection.onmessage = (event) => {
+    let data = JSON.parse(event.data);
+
+    if (!controller.data) {
+      controller.data = [];
+    }
+
+    controller.data.push(data);
+
+    console.table(controller.data, ["corrente", "potencia"]);
+  };
+
+  connection.addEventListener("open", function (event) {
+    console.info("Conexão WebSocket aberta");
+    
+    btnON.onclick = (event) => sendRequest("L");
+
+    btnOFF.onclick = (event) => sendRequest("D");
+
+    btnVERIFY.onclick = (event) => alert(controller.message);
+  });
+
+  connection.addEventListener("close", function (event) {
+    console.warn("Conexão WebSocket fechada");
+  });
+
+  connection.addEventListener("error", function (event) {
+    console.error("Erro WebSocket:", event);
+  });
+} else {
+  console.error("Seu navegador não suporta WebSocket.");
 }
-
-// Adicione um manipulador de eventos para mensagens recebidas
-webSocket.onmessage = processarMensagem;
