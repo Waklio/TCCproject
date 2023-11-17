6372953eacc5814a8c018ebbcc05aa1638165a06
Your Name <you@example.com>
2023-11-17 15:31:52 -0300

    refractor: Estado do relé
    Atualiza a recepção de informação do relé.

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const urlServer = "ws://192.168.43.100:81";
 const browserSupportsWebsocket = "WebSocket" in window;
 
 let controller = {
+  relayState: null,
   data: null,
   get message() {
     if (!this.data) {
@@ -14,7 +15,7 @@ let controller = {
 
     let last = this.data[this.data.length - 1];
 
-    return `corrente: ${last.corrente} A \n potencia: ${last.potencia} W`;
+    return `Corrente: ${last.eletricCurrent} A \n Potência: ${last.eletricPower} W`;
   },
 };
 
@@ -26,20 +27,22 @@ if (browserSupportsWebsocket) {
   }
 
   connection.onmessage = (event) => {
-    let data = JSON.parse(event.data);
+    let message = JSON.parse(event.data);
 
     if (!controller.data) {
       controller.data = [];
     }
 
-    controller.data.push(data);
+    controller.data.push(message.data);
+    controller.relayState = message.relayState;
 
-    console.table(controller.data, ["corrente", "potencia"]);
+    console.log("status do relé: " + controller.relayState);
+    console.table(controller.data, ["eletricCurrent", "eletricPower"]);
   };
 
   connection.addEventListener("open", function (event) {
     console.info("Conexão WebSocket aberta");
-    
+
     btnON.onclick = (event) => sendRequest("L");
 
     btnOFF.onclick = (event) => sendRequest("D");
