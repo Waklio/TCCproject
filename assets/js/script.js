7e4d11502cac0e30b028ed90c7dadab0b3619198
Your Name <you@example.com>
2023-11-17 17:25:40 -0300

    feat: Implementação do Gráfico
    Formatação GERAL

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,16 +8,67 @@ const browserSupportsWebsocket = "WebSocket" in window;
 let controller = {
   relayState: null,
   data: null,
+  get lastDataIndex() {
+    if (!this.data) {
+      return 0;
+    }
+
+    return this.data.length - 1;
+  },
+  get lastData() {
+    return this.data[this.lastDataIndex];
+  },
   get message() {
     if (!this.data) {
       return `não tem dados`;
     }
-
-    let last = this.data[this.data.length - 1];
-
-    return `Corrente: ${last.eletricCurrent} A \n Potência: ${last.eletricPower} W`;
+    return `Corrente: ${this.lastData.eletricCurrent} A \n Potência: ${this.lastData.eletricPower} W`;
   },
 };
+
+let chartLabels = [];
+let eletricPowerData = [];
+let chartData = {
+  labels: chartLabels,
+  datasets: [
+    {
+      label: "Potência em W",
+      data: eletricPowerData,
+      borderColor: "#d40e0e",
+      backgroundColor: "#d40e0e50",
+    },
+  ],
+};
+
+const config = {
+  type: "line",
+  data: chartData,
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "top",
+      },
+      title: {
+        display: true,
+        text: "Consumo",
+      },
+    },
+  },
+};
+const ctx = document.getElementById("chart-consumption");
+let chart = new Chart(ctx, config);
+
+function addToChart(chart, label, value) {
+  chart.data.labels.push(label);
+  chart.data.datasets[chart.data.datasets.length - 1].data.push(value);
+  chart.update();
+}
 
 if (browserSupportsWebsocket) {
   let connection = new WebSocket(urlServer);
@@ -35,6 +86,12 @@ if (browserSupportsWebsocket) {
 
     controller.data.push(message.data);
     controller.relayState = message.relayState;
+
+    addToChart(
+      chart,
+      controller.lastDataIndex,
+      controller.lastData.eletricPower
+    );
 
     console.log("status do relé: " + controller.relayState);
     console.table(controller.data, ["eletricCurrent", "eletricPower"]);
